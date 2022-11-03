cfb98c70f2ad1242d03266eb6d8784ce6fb9fea1
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-03 19:39:58 +0000

    lab10

---
+++ 1-programming/Assignments/labs/lab10/lab10.scm
@@ -0,0 +1,17 @@
+(define (over-or-under num1 num2)
+  (cond 
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+    (else          0)))
+
+(define (make-adder num) (lambda (i) (+ num i)))
+
+(define (composed f g) (lambda (x) (f(g x)))) 
+
+(define lst `((1) 2 (3 4) 5))
+
+(define (duplicate lst) 
+    (if (or (null? lst) (null? (cdr lst)))
+        lst
+        (duplicate (cdr (cdr ((cons (car lst) (append (list (car lst)) (cdr lst)))))))))
+
