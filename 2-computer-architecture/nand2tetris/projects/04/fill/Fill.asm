334428d76bf10a97457766d2582bd09cc6c6a498
Graham Theobald <Graham_Theobald@live.co.uk>
2022-12-12 16:01:11 +0000

    project 4

--- 2-computer-architecture/nand2tetris/projects/04/fill/Fill.asm
+++ 2-computer-architecture/nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,75 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(START)
+  @KBD 
+  D = M
+  @BLACK
+  D;JNE
+  @WHITE 
+  D;JEQ
+
+(BLACK)
+  @color 
+  M = -1
+  @PAINT
+  0;JMP
+
+(WHITE)
+  @color
+  M = 0
+  @PAINT
+  0;JMP
+
+(PAINT)
+  @SCREEN 
+  D = A 
+  @pixel 
+  M = D 
+  @rownumber 
+  M = 1
+  @ROWS
+  0;JMP
+
+(ROWS)
+  @rownumber 
+  D = M
+  @256
+  D = D - A
+  @START 
+  D;JGT
+  @colnumber 
+  M = 1
+  @COLS 
+  0;JMP
+  
+
+(COLS)
+  @colnumber 
+  D = M
+  @32 
+  D = D - A
+  @ROWINC 
+  D;JGT
+
+  @color
+  D = M
+  @pixel 
+  A = M 
+  M = D
+
+  @pixel 
+  M = M + 1
+
+  @colnumber 
+  M = M + 1
+
+  @COLS 
+  0;JMP
+
+(ROWINC) 
+  @rownumber
+  M = M + 1
+  @ROWS 
+  0;JMP
--- 2-computer-architecture/nand2tetris/projects/04/mult/Mult.asm
+++ 2-computer-architecture/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,32 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+  @count 
+  M = 1
+  @R2
+  M = 0
+
+(LOOP)
+  @count
+  D = M
+  @R0
+  D = D - M
+  @END
+  D;JGT
+
+  @R1
+  D = M
+  @R2
+  M = D + M
+
+  @count
+  M = M + 1
+
+  @LOOP
+  0;JMP
+
+
+
+(END)
+@END
+0;JMP
